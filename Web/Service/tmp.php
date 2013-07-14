ce8baa30a1f8717a87e50941b827610ea6402a70
jonniedarko <jonnie.maguire@gmail.com>
2013-07-14 20:58:08 +0100

    added Service Folder

---
+++ Web/Service/tmp.php
@@ -0,0 +1,25 @@
+<?php
+
+// Prevent caching.
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 01 Jan 1996 00:00:00 GMT');
+
+// The JSON standard MIME header.
+header('Content-type: application/json');
+
+// This ID parameter is sent by our javascript client.
+$id = $_GET['id'];
+
+// Here's some data that we want to send via JSON.
+// We'll include the $id parameter so that we
+// can show that it has been passed in correctly.
+// You can send whatever data you like.
+$data = array("Hello", $id);
+
+//$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+//echo json_encode($arr);
+
+// Send the data.
+echo json_encode($data);
+
+?>
