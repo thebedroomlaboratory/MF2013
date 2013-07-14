8855dbdf958b2e341725dc5b01a4fa805f8f6738
catchmartin <catchmartin@gmail.com>
2013-07-14 18:37:20 +0100

    All variables for serial with multiple arduinos configured

--- ControlScripts/SerialTest.py
+++ ControlScripts/SerialTest.py
@@ -5,57 +5,96 @@ from time import sleep
 from sys import exit
 import serial
 
-print("Setting up USB serial connection")
-serialFromArduino = serial.Serial("COM4",9600)
-serialFromArduino.flush()
-print("Ready to read!")
-startup = True
-posInVal = 0
-posOfVal = 1
-values = [ 5, 0.0, None, 0, None ]
-tempString = ""
-tempFloat = 0.0
-temperature = 0.0
-tempLow=0
-tempHigh=0
-tempRight=0
-heatingOn=True
-buttonCount=0
-buttonOverrideActive=False
+print("Setting up variables and USB serial connections")
 
-webOn=False
-webCount=0
-webOverrideActive=False
-while True:
-    try:
-        val = serialFromArduino.read()
-        print(val)
+# Global Variables for people counter stuff
+# This arduino number is 0
+pplSerial = serial.Serial("COM10",9600)
+pplSerial.flush()
+pplStartup = True
+pplPosOfVal = 0
+pplTempString = ""
+pplArray = [ 0, 1 ]
+
+# Global Variables for Thermostat
+# This arduino number is 1
+thermSerial = serial.Serial("COM10",9600)
+thermSerial.flush()
+thermStartup = True
+thermPosOfVal = 0
+thermTempString = ""
+thermArray = [ 0, 1 ]
+
+# Global Variables for Bagel Oven
+# This arduino number is 2
+ovenSerial = serial.Serial("COM10",9600)
+ovenSerial.flush()
+ovenStartup = True
+ovenPosOfVal = 0
+ovenTempString = ""
+ovenArray = [ 0, 1 ]
+
+
+print("Ready to read!")
+
+def readSerial(thisArduino, thisSerial, thisStartup, thisPosOfValue, thisTempString, thisArray):    
+    if (thisSerial.inWaiting() > 0):
+        val = thisSerial.read()
         if (val == '\n'):
-            if (startup == True):
-                startup = False
-            elif (startup == False):
-                #print temp, light
-                values [posOfVal] = int(tempString)
-                print(tempString)
-                print(values)
+            if (thisStartup == True):
+                print("hi");
+                thisStartup = False
+            elif (thisStartup == False):
+                thisArray [thisPosOfValue] = int(thisTempString)
+                print(thisArray)
+                thisTempString = ""
+                thisPosOfValue = 0
+                exit()
         elif (val == '\r'):
             i=0		
         elif (val == '?'):
-            if (startup == False):
-                tempFloat = float(int(tempString))
-                tempFloat /= 10
-                tempFloat -= 7
-                temperature = tempFloat
-                values [posOfVal] = tempFloat
-                print(tempString)
-                tempString = ""
-                posInVal = 0
-                posOfVal += 2
+            if (thisStartup == False):
+                thisArray [thisPosOfValue] = int(thisTempString)
+                thisTempString = ""
+                thisPosOfValue += 1
         else:
-            if (startup == False):
-                tempString += str(val)
-            else:
-                print("aaagh")
+            if (thisStartup == False):
+                thisTempString += str(val)
+        if(thisArduino==0):
+            global pplStartup
+            pplStartup = thisStartup
+            global pplPosOfVal
+            pplPosOfVal = thisPosOfValue
+            global pplTempString
+            pplTempString = thisTempString
+            global pplArray
+            pplArray = thisArray
+        elif(thisArduino==1):
+            global thermStartup
+            thermStartup = thisStartup
+            global thermPosOfVal
+            thermPosOfVal = thisPosOfValue
+            global thermTempString
+            thermTempString = thisTempString
+            global thermArray
+            thermArray = thisArray
+        elif(thisArduino==2):
+            global ovenStartup
+            ovenStartup = thisStartup
+            global ovenPosOfVal
+            ovenPosOfVal = thisPosOfValue
+            global ovenTempString
+            ovenTempString = thisTempString
+            global ovenArray
+            ovenArray = thisArray
+            
 
+
+while True:
+    try:
+        readSerial(0, pplSerial, pplStartup, pplPosOfVal, pplTempString, pplArray)
+        print(pplArray)
+        
     except KeyboardInterrupt: 
         exit()
+    
