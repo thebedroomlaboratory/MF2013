4130ee5abb48e5341ce2602e2d32b285df3ab230
catchmartin <catchmartin@gmail.com>
2013-07-16 23:13:49 +0100

    added check for when a serial reading is complete

--- ControlScripts/Python/SerialTest.py
+++ ControlScripts/Python/SerialTest.py
@@ -9,92 +9,113 @@ print("Setting up variables and USB serial connections")
 
 # Global Variables for people counter stuff
 # This arduino number is 0
-pplSerial = serial.Serial("COM10",9600)
+pplSerial = serial.Serial("COM5",9600)
 pplSerial.flush()
 pplStartup = True
 pplPosOfVal = 0
 pplTempString = ""
 pplArray = [ 0, 1 ]
+pplReadingComplete = False
 
 # Global Variables for Thermostat
 # This arduino number is 1
-thermSerial = serial.Serial("COM10",9600)
-thermSerial.flush()
+#thermSerial = serial.Serial("COM10",9600)
+#thermSerial.flush()
 thermStartup = True
 thermPosOfVal = 0
 thermTempString = ""
 thermArray = [ 0, 1 ]
+thermReadingComplete = False
 
 # Global Variables for Bagel Oven
 # This arduino number is 2
-ovenSerial = serial.Serial("COM10",9600)
-ovenSerial.flush()
+#ovenSerial = serial.Serial("COM10",9600)
+#ovenSerial.flush()
 ovenStartup = True
 ovenPosOfVal = 0
 ovenTempString = ""
 ovenArray = [ 0, 1 ]
+ovenReadingComplete = False
 
 
 print("Ready to read!")
 
-def readSerial(thisArduino, thisSerial, thisStartup, thisPosOfValue, thisTempString, thisArray):    
+def readSerial(thisArduino, thisSerial, thisStartup, thisPosOfValue, thisTempString, thisArray, thisReadingComplete):    
     if (thisSerial.inWaiting() > 0):
         val = thisSerial.read()
         if (val == '\n'):
             if (thisStartup == True):
-                print("hi");
                 thisStartup = False
             elif (thisStartup == False):
                 thisArray [thisPosOfValue] = int(thisTempString)
-                print(thisArray)
                 thisTempString = ""
                 thisPosOfValue = 0
-                exit()
+                thisReadingComplete = True
         elif (val == '\r'):
-            i=0		
+            thisReadingComplete = False
         elif (val == '?'):
             if (thisStartup == False):
                 thisArray [thisPosOfValue] = int(thisTempString)
                 thisTempString = ""
                 thisPosOfValue += 1
+                thisReadingComplete = False
         else:
             if (thisStartup == False):
                 thisTempString += str(val)
+                thisReadingComplete = False
         if(thisArduino==0):
             global pplStartup
-            pplStartup = thisStartup
             global pplPosOfVal
-            pplPosOfVal = thisPosOfValue
             global pplTempString
-            pplTempString = thisTempString
             global pplArray
+            global pplReadingComplete
+            pplStartup = thisStartup
+            pplPosOfVal = thisPosOfValue
+            pplTempString = thisTempString
             pplArray = thisArray
+            pplReadingComplete = thisReadingComplete
         elif(thisArduino==1):
             global thermStartup
-            thermStartup = thisStartup
             global thermPosOfVal
-            thermPosOfVal = thisPosOfValue
             global thermTempString
-            thermTempString = thisTempString
             global thermArray
+            global thermReadingComplete
+            thermStartup = thisStartup
+            thermPosOfVal = thisPosOfValue
+            thermTempString = thisTempString
             thermArray = thisArray
+            thermReadingComplete = thisReadingComplete
         elif(thisArduino==2):
             global ovenStartup
-            ovenStartup = thisStartup
             global ovenPosOfVal
-            ovenPosOfVal = thisPosOfValue
             global ovenTempString
-            ovenTempString = thisTempString
             global ovenArray
+            global ovenReadingComplete
+            ovenStartup = thisStartup
+            ovenPosOfVal = thisPosOfValue
+            ovenTempString = thisTempString
             ovenArray = thisArray
+            ovenReadingComplete = thisReadingComplete
+    else:
+        thisReadingComplete = False
+        if(thisArduino==0):
+            global pplReadingComplete
+            pplReadingComplete = thisReadingComplete
+        elif(thisArduino==1):
+            global thermReadingComplete
+            thermReadingComplete = thisReadingComplete
+        elif(thisArduino==2):
+            global ovenReadingComplete
+            ovenReadingComplete = thisReadingComplete
             
-
+def serialWrite():
+    print("Ready to read!")
 
 while True:
     try:
-        readSerial(0, pplSerial, pplStartup, pplPosOfVal, pplTempString, pplArray)
-        print(pplArray)
-        
+        readSerial(0, pplSerial, pplStartup, pplPosOfVal, pplTempString, pplArray, pplReadingComplete)
+        if(pplReadingComplete):
+            print(pplArray)
     except KeyboardInterrupt: 
         exit()
     
