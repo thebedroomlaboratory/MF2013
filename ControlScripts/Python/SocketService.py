1073196419ceb1a3fb9f564fcf2e2d2a249a2f7f
jonniedarko <jonnie.maguire@gmail.com>
2013-07-16 17:37:35 +0100

    SocketService.py - cleaned up and removed unnecessary code
    Removed unnecessary code not needed

--- ControlScripts/Python/SocketService.py
+++ ControlScripts/Python/SocketService.py
@@ -2,17 +2,11 @@
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
-import tornado.template
-
-class MainHandler(tornado.web.RequestHandler):
-  def get(self):
-    loader = tornado.template.Loader(".")
-    self.write(loader.load("index.html").generate())
 
 class WSHandler(tornado.websocket.WebSocketHandler):
   def open(self):
     print 'connection opened...'
-    self.write_message("The server says: 'Hello'. Connection was accepted.")
+    self.write_message("Connection was accepted.")
 
   def on_message(self, message):
     self.write_message("Updating...")
@@ -26,16 +20,11 @@ class WSHandler(tornado.websocket.WebSocketHandler):
     #updateOvenStatus(oven)
     self.write_message("Updated")
 
-
-    
-
   def on_close(self):
     print 'connection closed...'
 
 application = tornado.web.Application([
   (r'/ws', WSHandler),
-  (r'/', MainHandler),
-  (r"/(.*)", tornado.web.StaticFileHandler, {"path": "./resources"}),
 ])
 
 if __name__ == "__main__":
