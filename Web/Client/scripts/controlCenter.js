39e78d681997184f9b616331ee2ff821d91d129f
ColinGreene <colin.greene130@gmail.com>
2013-07-21 13:51:42 +0100

    Now transport information via JSON

--- Web/Client/scripts/controlCenter.js
+++ Web/Client/scripts/controlCenter.js
@@ -1,27 +1,49 @@
-/*  	Control Center related functions	*/
-function getCommands(){
-	var isHeatOn = $('#heat_onoffswitch').is(':checked')  === true?2:0;
-	var isLightOn = $('#light_onoffswitch').is(':checked') === true?1:0;
-	var isOvenOn = $('#oven_onoffswitch').is(':checked') === true?1:0;
-	return String(isHeatOn)+String(isLightOn)+String(isOvenOn);		
-}//End of getCommands()
+function getSliderValue(id){
+  return $('#'+id).is(':checked') === true;
+}
+
+function getInputDomObjects(){
+  return $("input:checkbox");
+}
+
+function getIdsFromSocketInputs(){
+  var sliderInputs = getInputDomObjects();
+  var ids = new Array();
+  for(var i=0; i<sliderInputs.length; i++){
+    // input has socketSwitch class
+    if($(sliderInputs[i]).hasClass('socketSwitch')){
+      ids[i] = sliderInputs[i].id;
+    }
+  }
+  return ids;
+}
+
+function populateJsonMessage(){
+  var jsonObj = {};
+  var ids = getIdsFromSocketInputs();
+  for(var i=0; i<ids.length; i++){
+    jsonObj[ids[i]] = getSliderValue(ids[i]) ;
+  }
+  return JSON.stringify(jsonObj);
+}
 
 function sendCommands()
 {
-  	commands = getCommands();	
-    var connection=new WebSocket("ws://localhost:9090/ws");
-    connection.onopen = function () {
-      connection.send(commands); //send a message to server once connection is opened.
-    };
-    connection.onerror = function (error) {
-      console.log('Error Logged: ' + error); //log errors
-    };
-    connection.onmessage = function (e) {
-      console.log('Received From Server: ' + e.data); //log the received message
-    };
-    connection.onclose = function(){
-    	console.log('connection closed');
-    };
+  var jsonMessage = populateJsonMessage();
+  console.log(jsonMessage);
+  var connection=new WebSocket("ws://localhost:9090/ws");
+  connection.onopen = function () {
+    connection.send(jsonMessage); //send a message to server once connection is opened.
+  };
+  connection.onerror = function (error) {
+    console.log('Error Logged: ' + error); //log errors
+  };
+  connection.onmessage = function (e) {
+    console.log('Received From Server: ' + e.data); //log the received message
+  };
+  connection.onclose = function(){
+    console.log('connection closed');
+  };
 }//End of sendCommands()
 
 /*		End of Control Center related functions		*/
