0a83de8e09a2183a9fdac2a48851a2299c4cd91a
jonniedarko <jonnie.maguire@gmail.com>
2013-07-21 19:14:27 +0100

    Update maker.js
    Added Chart and Light functions

--- Web/Client/scripts/maker.js
+++ Web/Client/scripts/maker.js
@@ -1,12 +1,4 @@
-//attaches sendCommands function to trigger when any button of class "socketSwitch" changes status
-$(function(ready){
-    $('.socketSwitch').change(function() {
-        sendCommands();
-    });
-});
-
-
-
+/*
 // This just displays the first parameter passed to it
 // in an alert.
 function show(json) {
@@ -20,3 +12,69 @@ function run() {
 	show // The function to call on completion.
 	);
 }
+*/
+
+$(function(ready){
+    $('.socketSwitch').change(function() {
+        sendCommands();
+    });
+
+    populateGraphs();
+
+});
+
+
+function populateGraphs(){
+
+	//Thermostat
+	//Get the context of the canvas element we want to select
+	var thermostat_ctx = $("#thermostat_Line_chart").get(0).getContext("2d");
+	//var myNewChart = new Chart(thermostat_ctx);
+	var data = {
+	labels : ["January","February","March","April","May","June","July"],
+	datasets : [
+					{
+						fillColor : "rgba(151,187,205,0.5)",
+						strokeColor : "rgba(151,187,205,1)",
+						pointColor : "rgba(151,187,205,1)",
+						pointStrokeColor : "#fff",
+						data : [28,48,40,19,96,27,100]
+					}
+				]
+			}
+	var myNewChart = new Chart(thermostat_ctx).Line(data);
+	//fitToContainer(document.querySelector('canvas'));
+
+
+	//Oven
+	var data2 = [
+	{
+		value: 30,
+		color:"white"
+	},
+	{
+		value : 120,
+		color : "red"
+	}
+
+]
+	//Thermostat
+	//Get the context of the canvas element we want to select
+	var oven_ctx = $("#Oven_Line_chart").get(0).getContext("2d");
+	//var myNewChart = new Chart(thermostat_ctx);
+	var ovenChart = new Chart(oven_ctx).Doughnut(data2);
+	fitToContainer(document.querySelector('canvas'));
+
+	var ovenTempChart_ctx = $("#oven_temp_chart").get(0).getContext("2d");
+	var ovenTempChart = new Chart(ovenTempChart_ctx).Line(data);
+
+
+}
+
+function fitToContainer(canvas){
+  canvas.style.width='100%';
+  canvas.style.height='100%';
+  canvas.width  = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+}
+
